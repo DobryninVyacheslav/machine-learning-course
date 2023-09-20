07ef9feb4c1d2fba88d9dcc4bfab0fed01e2d395
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-09-20 21:38:29 +0400

    feat: add points and connections

--- lessons/lesson_2/visualization/hnsw.py
+++ lessons/lesson_2/visualization/hnsw.py
@@ -46,17 +46,79 @@ class HNSW(ThreeDScene):
         # layer_2.set_fill_by_value(axes=axes, colorscale=[(RED, -0.5), (YELLOW, 0), (GREEN, 0.5)], axis=2)
         # self.add(axes)
 
-        self.add(
-            layer_0,
-            layer_1,
+        e_p_l2 = get_circle(axes.c2p(2, 4.5, 1))
+        p2_l2 = get_circle(axes.c2p(2.1, 2, 1))
+        group_2 = VGroup(
             layer_2,
-            get_circle(axes.c2p(2, 4.5, 1)),
-            get_circle(axes.c2p(2, 4.5, 0.5)),
-            get_circle(axes.c2p(2, 4.5, -0.25)),
-            get_circle(axes.c2p(2.1, 2, 1)),
-            get_circle(axes.c2p(2.1, 2, 0.5)),
-            get_circle(axes.c2p(2.1, 2, -0.25)),
+            e_p_l2,
+            p2_l2,
         )
+        p1_l1 = get_circle(axes.c2p(2, 4.5, 0.5))
+        p2_l1 = get_circle(axes.c2p(3.5, 4.6, 0.5))
+        p3_l1 = get_circle(axes.c2p(3, 3.9, 0.5))
+        p4_l1 = get_circle(axes.c2p(1.8, 2.8, 0.5))
+        p5_l1 = get_circle(axes.c2p(2.1, 2, 0.5))
+        p6_l1 = get_circle(axes.c2p(3.4, 2.5, 0.5))
+        group_1 = VGroup(
+            layer_1,
+            Line(p1_l1.get_center(), p2_l1.get_center(), color=GRAY),
+            Line(p1_l1.get_center(), p3_l1.get_center(), color=GRAY),
+            Line(p2_l1.get_center(), p3_l1.get_center(), color=GRAY),
+            Line(p3_l1.get_center(), p4_l1.get_center(), color=GRAY),
+            Line(p4_l1.get_center(), p5_l1.get_center(), color=GRAY),
+            p1_l1,
+            p2_l1,
+            p3_l1,
+            p4_l1,
+            p5_l1,
+            p6_l1,
+        )
+        p1_l0 = get_circle(axes.c2p(2, 4.5, -0.25))
+        p2_l0 = get_circle(axes.c2p(3.5, 4.6, -0.25))
+        p3_l0 = get_circle(axes.c2p(3, 3.9, -0.25))
+        p4_l0 = get_circle(axes.c2p(2, 3.8, -0.25))
+        p5_l0 = get_circle(axes.c2p(1.8, 2.8, -0.25))
+        p6_l0 = get_circle(axes.c2p(2.1, 2, -0.25))
+        p7_l0 = get_circle(axes.c2p(3.4, 2.5, -0.25))
+        p8_l0 = get_circle(axes.c2p(2.8, 3, -0.25))
+        p9_l0 = get_circle(axes.c2p(3.5, 3.2, -0.25))
+        group_0 = VGroup(
+            layer_0,
+            Line(p1_l0.get_center(), p4_l0.get_center(), color=GRAY),
+            Line(p2_l0.get_center(), p4_l0.get_center(), color=GRAY),
+            Line(p3_l0.get_center(), p4_l0.get_center(), color=GRAY),
+            Line(p4_l0.get_center(), p5_l0.get_center(), color=GRAY),
+            Line(p4_l0.get_center(), p8_l0.get_center(), color=GRAY),
+            Line(p5_l0.get_center(), p8_l0.get_center(), color=GRAY),
+            Line(p6_l0.get_center(), p8_l0.get_center(), color=GRAY),
+            Line(p7_l0.get_center(), p8_l0.get_center(), color=GRAY),
+            p1_l0,
+            p2_l0,
+            p3_l0,
+            p4_l0,
+            p5_l0,
+            p6_l0,
+            p7_l0,
+            p8_l0,
+            p9_l0,
+        )
+        self.add(
+            group_2,
+            group_1,
+            group_0,
+            DashedLine(e_p_l2.get_center(), p1_l1.get_center()),
+            DashedLine(p6_l1.get_center(), p7_l0.get_center()),
+            DashedLine(p5_l1.get_center(), p6_l0.get_center()),
+            DashedLine(p4_l1.get_center(), p5_l0.get_center()),
+            DashedLine(p2_l1.get_center(), p2_l0.get_center()),
+            DashedLine(p1_l1.get_center(), p1_l0.get_center()),
+        )
+        self.play(Create(Arrow(e_p_l2.get_center(), p2_l2.get_center(), stroke_width=2)))
+        self.play(Create(Arrow(p2_l2.get_center(), p5_l1.get_center())))
+        self.play(Create(Arrow(p5_l1.get_center(), p6_l1.get_center())))
+        self.play(Create(Arrow(p6_l1.get_center(), p3_l1.get_center())))
+        self.play(Create(Arrow(p3_l1.get_center(), p3_l0.get_center())))
+        self.play(Create(Arrow(p3_l0.get_center(), p8_l0.get_center())))
 
 
 if __name__ == '__main__':
